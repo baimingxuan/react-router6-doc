1786c7d2c6142c4d45d09b324a53bcb264aa8360
baimingxuan <354372738@qq.com>
2023-11-05 11:50:15 +0000

    Deploying to gh-pages from @ baimingxuan/react-router6-doc@46203df420efdc9b54984cae3ac5d96395e710e8 🚀

---
+++ assets/router_create-hash-router.md.bf96eb0c.js
@@ -0,0 +1,55 @@
+import{_ as s,o as a,c as n,Q as o}from"./chunks/framework.6f0990a9.js";const F=JSON.parse('{"title":"createHashRouter","description":"","frontmatter":{},"headers":[],"relativePath":"router/create-hash-router.md","filePath":"router/create-hash-router.md","lastUpdated":1699184954000}'),l={name:"router/create-hash-router.md"},p=o(`<h1 id="createhashrouter" tabindex="-1"><code>createHashRouter</code> <a class="header-anchor" href="#createhashrouter" aria-label="Permalink to &quot;\`createHashRouter\`&quot;">​</a></h1><p>如果您无法配置 Web 服务器将所有流量定向到 React Router 应用程序，那么这个路由就非常有用。它不使用普通 URL，而是使用 URL 的哈希 (#) 部分来管理 &quot;应用程序 URL&quot;。</p><blockquote><p>不建议使用哈希 URL。</p></blockquote><p>除此之外，它在功能上与<a href="https://baimingxuan.github.io/react-router6-doc/routers/create-browser-router" target="_blank" rel="noreferrer"><code>createBrowserRouter</code></a>相同。</p><div class="language-jsx vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang">jsx</span><pre class="shiki github-dark vp-code-dark"><code><span class="line"><span style="color:#F97583;">import</span><span style="color:#E1E4E8;"> </span><span style="color:#79B8FF;">*</span><span style="color:#E1E4E8;"> </span><span style="color:#F97583;">as</span><span style="color:#E1E4E8;"> React </span><span style="color:#F97583;">from</span><span style="color:#E1E4E8;"> </span><span style="color:#9ECBFF;">&quot;react&quot;</span><span style="color:#E1E4E8;">;</span></span>
+<span class="line"><span style="color:#F97583;">import</span><span style="color:#E1E4E8;"> </span><span style="color:#79B8FF;">*</span><span style="color:#E1E4E8;"> </span><span style="color:#F97583;">as</span><span style="color:#E1E4E8;"> ReactDOM </span><span style="color:#F97583;">from</span><span style="color:#E1E4E8;"> </span><span style="color:#9ECBFF;">&quot;react-dom&quot;</span><span style="color:#E1E4E8;">;</span></span>
+<span class="line"><span style="color:#F97583;">import</span><span style="color:#E1E4E8;"> {</span></span>
+<span class="line"><span style="color:#E1E4E8;">  createHashRouter,</span></span>
+<span class="line"><span style="color:#E1E4E8;">  RouterProvider,</span></span>
+<span class="line"><span style="color:#E1E4E8;">} </span><span style="color:#F97583;">from</span><span style="color:#E1E4E8;"> </span><span style="color:#9ECBFF;">&quot;react-router-dom&quot;</span><span style="color:#E1E4E8;">;</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#F97583;">import</span><span style="color:#E1E4E8;"> Root, { rootLoader } </span><span style="color:#F97583;">from</span><span style="color:#E1E4E8;"> </span><span style="color:#9ECBFF;">&quot;./routes/root&quot;</span><span style="color:#E1E4E8;">;</span></span>
+<span class="line"><span style="color:#F97583;">import</span><span style="color:#E1E4E8;"> Team, { teamLoader } </span><span style="color:#F97583;">from</span><span style="color:#E1E4E8;"> </span><span style="color:#9ECBFF;">&quot;./routes/team&quot;</span><span style="color:#E1E4E8;">;</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#F97583;">const</span><span style="color:#E1E4E8;"> </span><span style="color:#79B8FF;">router</span><span style="color:#E1E4E8;"> </span><span style="color:#F97583;">=</span><span style="color:#E1E4E8;"> </span><span style="color:#B392F0;">createHashRouter</span><span style="color:#E1E4E8;">([</span></span>
+<span class="line"><span style="color:#E1E4E8;">  {</span></span>
+<span class="line"><span style="color:#E1E4E8;">    path: </span><span style="color:#9ECBFF;">&quot;/&quot;</span><span style="color:#E1E4E8;">,</span></span>
+<span class="line"><span style="color:#E1E4E8;">    element: &lt;</span><span style="color:#79B8FF;">Root</span><span style="color:#E1E4E8;"> /&gt;,</span></span>
+<span class="line"><span style="color:#E1E4E8;">    loader: rootLoader,</span></span>
+<span class="line"><span style="color:#E1E4E8;">    children: [</span></span>
+<span class="line"><span style="color:#E1E4E8;">      {</span></span>
+<span class="line"><span style="color:#E1E4E8;">        path: </span><span style="color:#9ECBFF;">&quot;team&quot;</span><span style="color:#E1E4E8;">,</span></span>
+<span class="line"><span style="color:#E1E4E8;">        element: &lt;</span><span style="color:#79B8FF;">Team</span><span style="color:#E1E4E8;"> /&gt;,</span></span>
+<span class="line"><span style="color:#E1E4E8;">        loader: teamLoader,</span></span>
+<span class="line"><span style="color:#E1E4E8;">      },</span></span>
+<span class="line"><span style="color:#E1E4E8;">    ],</span></span>
+<span class="line"><span style="color:#E1E4E8;">  },</span></span>
+<span class="line"><span style="color:#E1E4E8;">]);</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#E1E4E8;">ReactDOM.</span><span style="color:#B392F0;">createRoot</span><span style="color:#E1E4E8;">(document.</span><span style="color:#B392F0;">getElementById</span><span style="color:#E1E4E8;">(</span><span style="color:#9ECBFF;">&quot;root&quot;</span><span style="color:#E1E4E8;">)).</span><span style="color:#B392F0;">render</span><span style="color:#E1E4E8;">(</span></span>
+<span class="line"><span style="color:#E1E4E8;">  &lt;</span><span style="color:#79B8FF;">RouterProvider</span><span style="color:#E1E4E8;"> </span><span style="color:#B392F0;">router</span><span style="color:#F97583;">=</span><span style="color:#E1E4E8;">{router} /&gt;</span></span>
+<span class="line"><span style="color:#E1E4E8;">);</span></span></code></pre><pre class="shiki github-light vp-code-light"><code><span class="line"><span style="color:#D73A49;">import</span><span style="color:#24292E;"> </span><span style="color:#005CC5;">*</span><span style="color:#24292E;"> </span><span style="color:#D73A49;">as</span><span style="color:#24292E;"> React </span><span style="color:#D73A49;">from</span><span style="color:#24292E;"> </span><span style="color:#032F62;">&quot;react&quot;</span><span style="color:#24292E;">;</span></span>
+<span class="line"><span style="color:#D73A49;">import</span><span style="color:#24292E;"> </span><span style="color:#005CC5;">*</span><span style="color:#24292E;"> </span><span style="color:#D73A49;">as</span><span style="color:#24292E;"> ReactDOM </span><span style="color:#D73A49;">from</span><span style="color:#24292E;"> </span><span style="color:#032F62;">&quot;react-dom&quot;</span><span style="color:#24292E;">;</span></span>
+<span class="line"><span style="color:#D73A49;">import</span><span style="color:#24292E;"> {</span></span>
+<span class="line"><span style="color:#24292E;">  createHashRouter,</span></span>
+<span class="line"><span style="color:#24292E;">  RouterProvider,</span></span>
+<span class="line"><span style="color:#24292E;">} </span><span style="color:#D73A49;">from</span><span style="color:#24292E;"> </span><span style="color:#032F62;">&quot;react-router-dom&quot;</span><span style="color:#24292E;">;</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#D73A49;">import</span><span style="color:#24292E;"> Root, { rootLoader } </span><span style="color:#D73A49;">from</span><span style="color:#24292E;"> </span><span style="color:#032F62;">&quot;./routes/root&quot;</span><span style="color:#24292E;">;</span></span>
+<span class="line"><span style="color:#D73A49;">import</span><span style="color:#24292E;"> Team, { teamLoader } </span><span style="color:#D73A49;">from</span><span style="color:#24292E;"> </span><span style="color:#032F62;">&quot;./routes/team&quot;</span><span style="color:#24292E;">;</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#D73A49;">const</span><span style="color:#24292E;"> </span><span style="color:#005CC5;">router</span><span style="color:#24292E;"> </span><span style="color:#D73A49;">=</span><span style="color:#24292E;"> </span><span style="color:#6F42C1;">createHashRouter</span><span style="color:#24292E;">([</span></span>
+<span class="line"><span style="color:#24292E;">  {</span></span>
+<span class="line"><span style="color:#24292E;">    path: </span><span style="color:#032F62;">&quot;/&quot;</span><span style="color:#24292E;">,</span></span>
+<span class="line"><span style="color:#24292E;">    element: &lt;</span><span style="color:#005CC5;">Root</span><span style="color:#24292E;"> /&gt;,</span></span>
+<span class="line"><span style="color:#24292E;">    loader: rootLoader,</span></span>
+<span class="line"><span style="color:#24292E;">    children: [</span></span>
+<span class="line"><span style="color:#24292E;">      {</span></span>
+<span class="line"><span style="color:#24292E;">        path: </span><span style="color:#032F62;">&quot;team&quot;</span><span style="color:#24292E;">,</span></span>
+<span class="line"><span style="color:#24292E;">        element: &lt;</span><span style="color:#005CC5;">Team</span><span style="color:#24292E;"> /&gt;,</span></span>
+<span class="line"><span style="color:#24292E;">        loader: teamLoader,</span></span>
+<span class="line"><span style="color:#24292E;">      },</span></span>
+<span class="line"><span style="color:#24292E;">    ],</span></span>
+<span class="line"><span style="color:#24292E;">  },</span></span>
+<span class="line"><span style="color:#24292E;">]);</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#24292E;">ReactDOM.</span><span style="color:#6F42C1;">createRoot</span><span style="color:#24292E;">(document.</span><span style="color:#6F42C1;">getElementById</span><span style="color:#24292E;">(</span><span style="color:#032F62;">&quot;root&quot;</span><span style="color:#24292E;">)).</span><span style="color:#6F42C1;">render</span><span style="color:#24292E;">(</span></span>
+<span class="line"><span style="color:#24292E;">  &lt;</span><span style="color:#005CC5;">RouterProvider</span><span style="color:#24292E;"> </span><span style="color:#6F42C1;">router</span><span style="color:#D73A49;">=</span><span style="color:#24292E;">{router} /&gt;</span></span>
+<span class="line"><span style="color:#24292E;">);</span></span></code></pre></div>`,5),e=[p];function t(r,c,E,y,i,u){return a(),n("div",null,e)}const m=s(l,[["render",t]]);export{F as __pageData,m as default};
