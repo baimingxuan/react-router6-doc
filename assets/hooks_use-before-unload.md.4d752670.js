5bc18b147f28f287a8111e0a0cc672b54bacadff
baimingxuan <354372738@qq.com>
2023-11-10 15:56:47 +0000

    Deploying to gh-pages from @ baimingxuan/react-router6-doc@99d4bcb1c607cd4c1a984a91d0c8309e7ecf47d7 🚀

---
+++ assets/hooks_use-before-unload.md.4d752670.js
@@ -0,0 +1,41 @@
+import{_ as s,o as a,c as n,Q as l}from"./chunks/framework.6f0990a9.js";const f=JSON.parse('{"title":"useBeforeUnload","description":"","frontmatter":{},"headers":[],"relativePath":"hooks/use-before-unload.md","filePath":"hooks/use-before-unload.md","lastUpdated":1699631755000}'),p={name:"hooks/use-before-unload.md"},o=l(`<h1 id="usebeforeunload" tabindex="-1"><code>useBeforeUnload</code> <a class="header-anchor" href="#usebeforeunload" aria-label="Permalink to &quot;\`useBeforeUnload\`&quot;">​</a></h1><p>该钩子只是 <code>window.onbeforeunload</code> 的一个辅助工具。在用户离开页面之前，将重要的应用程序状态保存在页面上（如浏览器的本地存储）可能会很有用。这样，如果用户回来，就可以恢复任何状态信息（恢复表单输入值等）。</p><div class="language-jsx vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang">jsx</span><pre class="shiki github-dark vp-code-dark"><code><span class="line"><span style="color:#F97583;">import</span><span style="color:#E1E4E8;"> { useBeforeUnload } </span><span style="color:#F97583;">from</span><span style="color:#E1E4E8;"> </span><span style="color:#9ECBFF;">&quot;react-router-dom&quot;</span><span style="color:#E1E4E8;">;</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#F97583;">function</span><span style="color:#E1E4E8;"> </span><span style="color:#B392F0;">SomeForm</span><span style="color:#E1E4E8;">() {</span></span>
+<span class="line"><span style="color:#E1E4E8;">  </span><span style="color:#F97583;">const</span><span style="color:#E1E4E8;"> [</span><span style="color:#79B8FF;">state</span><span style="color:#E1E4E8;">, </span><span style="color:#79B8FF;">setState</span><span style="color:#E1E4E8;">] </span><span style="color:#F97583;">=</span><span style="color:#E1E4E8;"> React.</span><span style="color:#B392F0;">useState</span><span style="color:#E1E4E8;">(</span><span style="color:#79B8FF;">null</span><span style="color:#E1E4E8;">);</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#E1E4E8;">  </span><span style="color:#6A737D;">// save it off before users navigate away</span></span>
+<span class="line"><span style="color:#E1E4E8;">  </span><span style="color:#B392F0;">useBeforeUnload</span><span style="color:#E1E4E8;">(</span></span>
+<span class="line"><span style="color:#E1E4E8;">    React.</span><span style="color:#B392F0;">useCallback</span><span style="color:#E1E4E8;">(() </span><span style="color:#F97583;">=&gt;</span><span style="color:#E1E4E8;"> {</span></span>
+<span class="line"><span style="color:#E1E4E8;">      localStorage.stuff </span><span style="color:#F97583;">=</span><span style="color:#E1E4E8;"> state;</span></span>
+<span class="line"><span style="color:#E1E4E8;">    }, [state])</span></span>
+<span class="line"><span style="color:#E1E4E8;">  );</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#E1E4E8;">  </span><span style="color:#6A737D;">// read it in when they return</span></span>
+<span class="line"><span style="color:#E1E4E8;">  React.</span><span style="color:#B392F0;">useEffect</span><span style="color:#E1E4E8;">(() </span><span style="color:#F97583;">=&gt;</span><span style="color:#E1E4E8;"> {</span></span>
+<span class="line"><span style="color:#E1E4E8;">    </span><span style="color:#F97583;">if</span><span style="color:#E1E4E8;"> (state </span><span style="color:#F97583;">===</span><span style="color:#E1E4E8;"> </span><span style="color:#79B8FF;">null</span><span style="color:#E1E4E8;"> </span><span style="color:#F97583;">&amp;&amp;</span><span style="color:#E1E4E8;"> localStorage.stuff </span><span style="color:#F97583;">!=</span><span style="color:#E1E4E8;"> </span><span style="color:#79B8FF;">null</span><span style="color:#E1E4E8;">) {</span></span>
+<span class="line"><span style="color:#E1E4E8;">      </span><span style="color:#B392F0;">setState</span><span style="color:#E1E4E8;">(localStorage.stuff);</span></span>
+<span class="line"><span style="color:#E1E4E8;">    }</span></span>
+<span class="line"><span style="color:#E1E4E8;">  }, [state]);</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#E1E4E8;">  </span><span style="color:#F97583;">return</span><span style="color:#E1E4E8;"> &lt;&gt;{</span><span style="color:#6A737D;">/*... */</span><span style="color:#E1E4E8;">}&lt;/&gt;;</span></span>
+<span class="line"><span style="color:#E1E4E8;">}</span></span></code></pre><pre class="shiki github-light vp-code-light"><code><span class="line"><span style="color:#D73A49;">import</span><span style="color:#24292E;"> { useBeforeUnload } </span><span style="color:#D73A49;">from</span><span style="color:#24292E;"> </span><span style="color:#032F62;">&quot;react-router-dom&quot;</span><span style="color:#24292E;">;</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#D73A49;">function</span><span style="color:#24292E;"> </span><span style="color:#6F42C1;">SomeForm</span><span style="color:#24292E;">() {</span></span>
+<span class="line"><span style="color:#24292E;">  </span><span style="color:#D73A49;">const</span><span style="color:#24292E;"> [</span><span style="color:#005CC5;">state</span><span style="color:#24292E;">, </span><span style="color:#005CC5;">setState</span><span style="color:#24292E;">] </span><span style="color:#D73A49;">=</span><span style="color:#24292E;"> React.</span><span style="color:#6F42C1;">useState</span><span style="color:#24292E;">(</span><span style="color:#005CC5;">null</span><span style="color:#24292E;">);</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#24292E;">  </span><span style="color:#6A737D;">// save it off before users navigate away</span></span>
+<span class="line"><span style="color:#24292E;">  </span><span style="color:#6F42C1;">useBeforeUnload</span><span style="color:#24292E;">(</span></span>
+<span class="line"><span style="color:#24292E;">    React.</span><span style="color:#6F42C1;">useCallback</span><span style="color:#24292E;">(() </span><span style="color:#D73A49;">=&gt;</span><span style="color:#24292E;"> {</span></span>
+<span class="line"><span style="color:#24292E;">      localStorage.stuff </span><span style="color:#D73A49;">=</span><span style="color:#24292E;"> state;</span></span>
+<span class="line"><span style="color:#24292E;">    }, [state])</span></span>
+<span class="line"><span style="color:#24292E;">  );</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#24292E;">  </span><span style="color:#6A737D;">// read it in when they return</span></span>
+<span class="line"><span style="color:#24292E;">  React.</span><span style="color:#6F42C1;">useEffect</span><span style="color:#24292E;">(() </span><span style="color:#D73A49;">=&gt;</span><span style="color:#24292E;"> {</span></span>
+<span class="line"><span style="color:#24292E;">    </span><span style="color:#D73A49;">if</span><span style="color:#24292E;"> (state </span><span style="color:#D73A49;">===</span><span style="color:#24292E;"> </span><span style="color:#005CC5;">null</span><span style="color:#24292E;"> </span><span style="color:#D73A49;">&amp;&amp;</span><span style="color:#24292E;"> localStorage.stuff </span><span style="color:#D73A49;">!=</span><span style="color:#24292E;"> </span><span style="color:#005CC5;">null</span><span style="color:#24292E;">) {</span></span>
+<span class="line"><span style="color:#24292E;">      </span><span style="color:#6F42C1;">setState</span><span style="color:#24292E;">(localStorage.stuff);</span></span>
+<span class="line"><span style="color:#24292E;">    }</span></span>
+<span class="line"><span style="color:#24292E;">  }, [state]);</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#24292E;">  </span><span style="color:#D73A49;">return</span><span style="color:#24292E;"> &lt;&gt;{</span><span style="color:#6A737D;">/*... */</span><span style="color:#24292E;">}&lt;/&gt;;</span></span>
+<span class="line"><span style="color:#24292E;">}</span></span></code></pre></div>`,3),e=[o];function t(c,r,E,y,i,u){return a(),n("div",null,e)}const F=s(p,[["render",t]]);export{f as __pageData,F as default};
